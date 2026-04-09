6c81423813e29b3af05ad8270aa9c38e3d05277a
shubdev <shub.mahadule@gmail.com>
2026-04-09 15:24:49 +0530

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- frontend/instagram/src/app/app.store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/auth/user.slice";
-
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
--- server.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.js";
-import connectDB from "./src/config/db.js";
-
-connectDB();
-
-app.listen(8080, () => {
-  console.log("server connect to port 8080");
-});
